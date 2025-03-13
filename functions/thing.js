0ffa9d72e7762949aa0b07bfa679caaa851bd046
tabrezakhtar <tabrez.akhtar1@gmail.com>
2025-03-13 17:06:56 +0000

    use an example api

--- functions/thing.js
+++ functions/thing.js
@@ -1,4 +1,12 @@
-const serverlessExpress = require('@vendia/serverless-express');
-const app = require('../app.js');
+// const serverlessExpress = require('@vendia/serverless-express');
+// const app = require('../app.js');
+
+// exports.handler = serverlessExpress({ app });
+
+const express = require('express');
+const serverlessExpress = require('@codegenie/serverless-express');
+
+const app = express();
+app.get('/api', (req, res) => res.send('Hello World!'));
 
 exports.handler = serverlessExpress({ app });
